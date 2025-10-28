dbcafe8811b95ea1e967a6f537ac9d899e2a620f
Rafael <rafaelv.boscato@hotmail.com>
2025-10-28 17:09:23 -0300

    test

--- src/ia/chatgpt.service.ts
+++ src/ia/chatgpt.service.ts
@@ -69,7 +69,7 @@ export class ChatGptService {
         console.log('entrou no nenhum-dos-dois');
       }
 
-      return 'Ok';
+      return 'Ok1';
     } catch (error: unknown) {
       console.error('Erro na API da OpenAI:', error);
       if (error && typeof error === 'object' && 'response' in error) {
